8e35302b5ebe376be13984a05c77903b09846671
buguno <brunnodesouzabezerra@gmail.com>
2022-03-30 23:13:55 -0300

    feat: adding auth routes

--- adote_me/urls.py
+++ adote_me/urls.py
@@ -23,4 +23,6 @@ urlpatterns = [
     path('', include(router.urls)),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
     path('admin/', admin.site.urls),
+    path('auth/', include('djoser.urls')),
+    path('auth/', include('djoser.urls.jwt')),
 ]
